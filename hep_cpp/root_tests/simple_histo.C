b3903793d3c98f0427326b972f372e472f7b5dec
Ludovico Massaccesi <62757383+lmassach@users.noreply.github.com>
2020-12-03 18:18:13 +0100

    Fixed last commit.

--- hep_cpp/root_tests/simple_histo.C
+++ hep_cpp/root_tests/simple_histo.C
@@ -5,23 +5,21 @@
 #include "TStyle.h"
 #endif
 
-namespace myns {
-    void simple_histo() {
-        gStyle->SetOptFit(111);
-        gStyle->SetOptStat(0);
-        TH1F *histo = new TH1F("histo", "Example histogram", 64, -8, 8);
-        TCanvas *c = new TCanvas("c", "Example canvas", 600, 450);
-        histo->SetBinErrorOption(TH1::EBinErrorOpt::kPoisson);
-        histo->FillRandom("gaus");
-        histo->Draw("E0");
+void simple_histo() {
+    gStyle->SetOptFit(111);
+    gStyle->SetOptStat(0);
+    TH1F *histo = new TH1F("histo", "Example histogram", 64, -8, 8);
+    TCanvas *c = new TCanvas("c", "Example canvas", 600, 450);
+    histo->SetBinErrorOption(TH1::EBinErrorOpt::kPoisson);
+    histo->FillRandom("gaus");
+    histo->Draw("E0");
 
-        TF1 *ff = new TF1("ff", "gaus", -8, 8);
-        ff->SetParameters(5000, 0, 1);
-        histo->Fit(ff, "ILQR");
+    TF1 *ff = new TF1("ff", "gaus", -8, 8);
+    ff->SetParameters(5000, 0, 1);
+    histo->Fit(ff, "ILQR");
 
-        c->SetLogy();
-        c->SetGrid();
-        c->SaveAs("simple_histo.png");
-        c->Draw();
-    }
+    c->SetLogy();
+    c->SetGrid();
+    c->SaveAs("simple_histo.png");
+    c->Draw();
 }
--- hep_cpp/root_tests/simple_histo_sharedlib.py
+++ hep_cpp/root_tests/simple_histo_sharedlib.py
@@ -5,7 +5,6 @@ import ROOT
 subprocess.run("g++ -fpic -shared simple_histo.C -o simple_histo.so "
                "`root-config --cflags --libs`", shell=True, check=True)
 
-# "Header" for the library. Also gInterpreter.Declare("code") can be used.
-ROOT.gInterpreter.ProcessLine("namespace myns { void simple_histo(); }")
+ROOT.gInterpreter.Declare("void simple_histo();")
 ROOT.gInterpreter.Load("./simple_histo.so")
-ROOT.myns.simple_histo()
+ROOT.simple_histo()
